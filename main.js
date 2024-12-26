2e2d63e0938f373cab73aa64e2084cca01e32500
Abdikani98 <abdikanishaarib>
2024-12-25 19:28:59 -0800

    new

---
+++ main.js
@@ -0,0 +1,16 @@
+let iconMenu = document.querySelector("#iconMenu")
+let menu = document.querySelector("#menu")
+let iconX = document.querySelector("#iconX")
+
+iconMenu.addEventListener("click", () =>{
+    menu.style.display = "block"
+    iconX.style.display = "block"
+    iconMenu.style.display = "none"
+})
+
+iconX.addEventListener("click", () =>{
+    menu.style.display = "none"
+    iconMenu.style.display = "block"
+    iconX.style.display = "none"
+    
+})
