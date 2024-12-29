9b51a3055ab15b824f94dacdfb14a06beaedb1bd
Abdikani98 <abdikanishaarib>
2024-12-29 10:10:55 -0800

    new

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 let iconMenu = document.querySelector("#iconMenu")
 let menu = document.querySelector("#menu")
-let iconX = document.querySelector("#iconX")
+// let iconX = document.querySelector("#iconX")
 
-iconMenu.addEventListener("click", () =>{
-    menu.style.display = "block"
-    iconX.style.display = "block"
-    iconMenu.style.display = "none"
+
+
+
+
+
+
+iconMenu.addEventListener("click", () => {
+    menu.classList.toggle("hidden")
 })
 
-iconX.addEventListener("click", () =>{
-    menu.style.display = "none"
-    iconMenu.style.display = "block"
-    iconX.style.display = "none"
+
+
+
+
+
+
+
+// iconMenu.addEventListener("click", () =>{
+//     menu.style.display = "block"
+//     iconX.style.display = "block"
+//     iconMenu.style.display = "none"
+// })
+
+// iconX.addEventListener("click", () =>{
+//     menu.style.display = "none"
+//     iconMenu.style.display = "block"
+//     iconX.style.display = "none"
     
-})
+// })
